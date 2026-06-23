77f1ce1b2286dca647ffc357c6740a3373ac1098
Kartikey_Singh <kkartikeysinghh@gmail.com>
2026-06-23 18:04:05 +0530

    react sol 5h: add useEffect in App component to send some more response direct from App component

--- lesson-05-solutions/5g/chatbot-project/src/App.jsx
+++ lesson-05-solutions/5g/chatbot-project/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Chatbot } from 'supersimpledev';
 import { ChatInput } from "./components/ChatInput";
 import ChatMessages from "./components/ChatMessages";
 import "./App.css";
@@ -7,6 +8,15 @@ import "./App.css";
 
 function App() {
   const [chatMessages, setChatMessages] = useState([]);
+
+  useEffect(() => {
+    Chatbot.addResponses({
+      'goodbye': 'Goodbye. Have a great day!',
+      'give me a unique id': function() {
+        return `Sure! Here's a unique ID: ${crypto.randomUUID()}`;
+      }
+    });
+  }, [])
 
   return (
     <div className="app-container">

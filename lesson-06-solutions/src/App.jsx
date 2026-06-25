8fbbd16ad622e3c34f7c1d84c1c9eb4a92f23674
Kartikey_Singh <kkartikeysinghh@gmail.com>
2026-06-25 13:37:19 +0530

    6a sol: split checkout header in used it in CheckoutPage.jsx

---
+++ lesson-06-solutions/src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router";
+import { HomePage } from "./pages/HomePage";
+import { CheckoutPage } from "./pages/CheckoutPage";
+import { OrdersPage } from "./pages/OrdersPage";
+import { TrackingPage } from "./pages/TrackingPage";
+import "./App.css";
+
+function App() {
+  return (
+    <Routes>
+
+      <Route index element={<HomePage />} />
+      {/* this 'index' is same as ' path="/" ' both used for index/home route */}
+      <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
+    </Routes>
+  );
+}
+
+export default App;

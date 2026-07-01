e969dded99ab9e202f89edaa0e4e4ac5497e19bc
Kartikey_Singh <kkartikeysinghh@gmail.com>
2026-07-01 23:20:37 +0530

    import original code to add code of challenges of lesson-07

---
+++ lesson-07-solutions/ecommerce-project/src/App.jsx
@@ -0,0 +1,37 @@
+import { Routes, Route } from "react-router";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { HomePage } from "./pages/home/HomePage";
+import { CheckoutPage } from "./pages/checkout/CheckoutPage";
+import { OrdersPage } from "./pages/orders/OrdersPage";
+import { TrackingPage } from "./pages/TrackingPage";
+import { ErrorPage } from "./pages/ErrorPage";
+import "./App.css";
+
+function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+
+    const fetchAppData = async () => {
+      const response = await axios.get("/api/cart-items?expand=product")
+        setCart(response.data);
+    }
+
+    fetchAppData();
+
+  }, []);
+
+  return (
+    <Routes>
+      <Route index element={<HomePage cart={cart} />} />
+      {/* this 'index' is same as ' path="/" ' both used for index/home route */}
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
+      <Route path="orders" element={<OrdersPage cart={cart} />} />
+      <Route path="tracking" element={<TrackingPage />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ lesson-07-solutions/ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Header } from "../../components/Header";
+import { OrderSummary } from "./OrderSummary";
+import { PaymentSummary } from "./PaymentSummary";
+import "./CheckoutPage.css";
+import "./CheckoutHeader.css";
+
+export function CheckoutPage({ cart = [] }) {
+  const [deliveryOptions, setdeliveryOptions] = useState([]);
+  const [paymentSummary, setPaymentSummary] = useState(null)
+
+  useEffect(() => {
+
+    const fetchCheckoutData = async () => {
+      let response = await axios.get("/api/delivery-options?expand=estimatedDeliveryTime")
+
+          setdeliveryOptions(response.data);
+
+  
+        response = await axios.get('/api/payment-summary')
+        
+          setPaymentSummary(response.data)
+  
+    };
+
+    fetchCheckoutData();
+
+  }, []);
+
+  return (
+    <>
+      <link rel="icon" type="image/svg+xml" href="cart-favicon.png" />
+      <title>Checkout</title>
+      <Header />
+
+      <div className="checkout-page">
+        <div className="page-title">Review your order</div>
+
+        <div className="checkout-grid">
+          
+          <OrderSummary cart={cart} deliveryOptions={deliveryOptions} />
+          
+          <PaymentSummary paymentSummary={paymentSummary}/>
+          
+
+        </div>
+      </div>
+    </>
+  );
+}

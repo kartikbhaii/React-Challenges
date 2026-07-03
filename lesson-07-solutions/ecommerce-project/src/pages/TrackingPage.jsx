ea539cbeae728041099d953d03e24a36d43e5829
Kartikey_Singh <kkartikeysinghh@gmail.com>
2026-07-03 19:16:56 +0530

    7L: update progress-bar on TrackingPage by calculating remaining time left to deliver or if delivered.

--- lesson-07-solutions/ecommerce-project/src/pages/TrackingPage.jsx
+++ lesson-07-solutions/ecommerce-project/src/pages/TrackingPage.jsx
@@ -31,6 +31,16 @@ export function TrackingPage({ cart }) {
     return orderProduct.productId === productId
   })
 
+  const totalDeliveryTimeMs = orderProduct.estimatedDeliveryTimeMs - order.orderTimeMs
+
+  const timePassedMs = dayjs().valueOf() - order.orderTimeMs
+
+  let deliveryPercentage = (timePassedMs / totalDeliveryTimeMs ) * 100
+
+  if(deliveryPercentage > 100) {
+    deliveryPercentage = 100;
+  }
+
 
   return (
     <>
@@ -63,7 +73,7 @@ export function TrackingPage({ cart }) {
           </div>
 
           <div className="progress-bar-container">
-            <div className="progress-bar"></div>
+            <div className="progress-bar" style={{width: `${deliveryPercentage}%`}} ></div>
           </div>
         </div>
       </div>

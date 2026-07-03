f8268ad0a4cc8b665f6d6f23f203e4a914c230a9
Kartikey_Singh <kkartikeysinghh@gmail.com>
2026-07-03 19:21:04 +0530

    7m: use js to show: if percent = 100; then 'Delivered on; , else 'Arriving on' using switch case.

--- lesson-07-solutions/ecommerce-project/src/pages/TrackingPage.jsx
+++ lesson-07-solutions/ecommerce-project/src/pages/TrackingPage.jsx
@@ -53,7 +53,7 @@ export function TrackingPage({ cart }) {
             View all orders
           </Link>
 
-          <div className="delivery-date">Arriving on {dayjs(orderProduct.estimatedDeliveryTimeMs).format('dddd, MMMM D')}</div>
+          <div className="delivery-date">{deliveryPercentage>100? "Arriving on" : "Delivered on"} {dayjs(orderProduct.estimatedDeliveryTimeMs).format('dddd, MMMM D')}</div>
 
           <div className="product-info">
             {orderProduct.product.name}
